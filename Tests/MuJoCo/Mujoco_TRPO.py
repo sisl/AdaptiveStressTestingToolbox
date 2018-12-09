3fde8199056e7e22f072aac0b83f25f865763df3
maxiaoba <maxiaoba@umich.edu>
2018-12-08 23:44:59 -0800

    add Mujoco

---
+++ Tests/MuJoCo/Mujoco_TRPO.py
@@ -0,0 +1,96 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"    #just use CPU
+
+# from sandbox.rocky.tf.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from sandbox.rocky.tf.envs.base import TfEnv
+from sandbox.rocky.tf.policies.gaussian_mlp_policy import GaussianMLPPolicy
+from sandbox.rocky.tf.policies.gaussian_lstm_policy import GaussianLSTMPolicy
+from sandbox.rocky.tf.optimizers.conjugate_gradient_optimizer import ConjugateGradientOptimizer, FiniteDifferenceHvp
+from rllab.misc import logger
+from rllab.envs.normalized_env import normalize
+from rllab.envs.env_spec import EnvSpec
+from sandbox.rocky.tf.envs.base import to_tf_space
+
+from rllab.envs.gym_env import GymEnv
+
+from mylab.algos.trpo import TRPO
+
+import os.path as osp
+import argparse
+# from example_save_trials import *
+import tensorflow as tf
+import joblib
+import math
+import numpy as np
+
+# Logger Params
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp_name', type=str, default='HalfCheetah-v1')
+parser.add_argument('--snapshot_mode', type=str, default="gap")
+parser.add_argument('--snapshot_gap', type=int, default=10)
+parser.add_argument('--log_dir', type=str, default='./Data/TRPO/Test')
+parser.add_argument('--args_data', type=str, default=None)
+args = parser.parse_args()
+
+# Create the logger
+log_dir = args.log_dir
+
+tabular_log_file = osp.join(log_dir, 'process.csv')
+text_log_file = osp.join(log_dir, 'text.csv')
+params_log_file = osp.join(log_dir, 'args.txt')
+
+logger.log_parameters_lite(params_log_file, args)
+logger.add_text_output(text_log_file)
+logger.add_tabular_output(tabular_log_file)
+prev_snapshot_dir = logger.get_snapshot_dir()
+prev_mode = logger.get_snapshot_mode()
+logger.set_snapshot_dir(log_dir)
+logger.set_snapshot_mode(args.snapshot_mode)
+logger.set_snapshot_gap(args.snapshot_gap)
+logger.set_log_tabular_only(False)
+logger.push_prefix("[%s] " % args.exp_name)
+
+seed = 0
+
+np.random.seed(seed)
+tf.set_random_seed(seed)
+with tf.Session() as sess:
+	# Create env
+	env = TfEnv(normalize(GymEnv(args.exp_name, record_video=False, record_log=False)))
+	# print(env.observation_space)
+	# print(env.action_space)
+	# Create policy
+	policy = GaussianMLPPolicy(
+	    name='ast_agent',
+	    env_spec=env.spec,
+	    hidden_sizes=(64, 32)
+	)
+	# policy = GaussianLSTMPolicy(name='lstm_policy',
+	#                             env_spec=ast_spec,
+	#                             hidden_dim=128,
+	#                             use_peepholes=True)
+	
+	params = policy.get_params()
+	sess.run(tf.variables_initializer(params))
+
+	# Instantiate the RLLAB objects
+	baseline = LinearFeatureBaseline(env_spec=env.spec)
+	# optimizer = ConjugateGradientOptimizer(hvp_approach=FiniteDifferenceHvp(base_eps=1e-5))
+
+	algo = TRPO(
+	    env=env,
+	    policy=policy,
+	    baseline=baseline,
+	    batch_size=4000,
+	    step_size=0.1,
+	    n_itr=25,
+	    store_paths=True,
+	    # optimizer= optimizer,
+	    max_path_length=1000,
+	    plot=False,
+	    )
+
+	algo.train(sess=sess, init_var=False)
+
+	
