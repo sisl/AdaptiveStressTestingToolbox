8176742aafd16b4541f592b82ed141d0a27f1ff9
maxiaoba <maxiaoba@umich.edu>
2019-01-21 14:05:49 -0800

    debug

--- Tests/LunarLander/lunarlander.py
+++ Tests/LunarLander/lunarlander.py
@@ -401,7 +401,7 @@ def heuristic(env, s):
 
 if __name__=="__main__":
     #env = LunarLander()
-    env = LunarLanderContinuous()
+    env = LunarLander()
     s = env.reset()
     total_reward = 0
     steps = 0
