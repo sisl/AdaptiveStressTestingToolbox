e3f7214d4721815a0757387df8743dcb281de32f
maxiaoba <maxiaoba@umich.edu>
2019-01-27 21:27:41 -0800

    add mountain car P0001 test

--- Tests/Plot/plot_avgtopk_formal.py
+++ Tests/Plot/plot_avgtopk_formal.py
@@ -1,52 +1,55 @@
 import csv
 import os.path
 import matplotlib 
-matplotlib.rcParams.update({'font.size': 20})
+matplotlib.rcParams.update({'font.size': 15})
 from matplotlib import pyplot as plt
 import numpy as np
 
 n_trial = 5
-top_k = 10
+top_k = 1
 batch_size = 4000
-max_step = 1e7#np.inf
+max_step = np.inf
 max_reward = np.inf
 min_reward = -np.inf
 
-prepath = "../"
-# exp = "CartPoleNd"
-exp = "Acrobot"
-policies = ["TRPO","MCTS_BV","GATRDStep1.0Fmax","PSMCTSStep1.0Ec1.414K0.5A0.5Qmax","PSMCTSTRCStep1.0Ec1.414K0.5A0.5Qmax"]
-plot_name = exp
-legends = ["TRPO","MCTSBV","GA","PSMCTS*","PSMCTS"]
-# colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
-
-# colors = []
-# for i in range(len(policies)):
-#     colors.append('C'+str(i))
+exp_name = 'MountainCar'#'CartPole'#'LunarLander'#'Acrobot'#'BipedalWalker'#
+exp_param = 'L100P00011'#'L100Th0612I01'#'L100I05'#'L100Th19999'#'L100TL25'#
+prepath = "../"+exp_name+"/Data/AST/Lexington/"+exp_param
+plot_path = "/Users/xiaobaima/Dropbox/SISL/PSMCTS/IJCAI/Plots/"
+policies = ["TRPO",\
+        "GATRDP100T20K3Step1.0Fmean",\
+        "PSMCTSTRCK0.5A0.5Ec1.414Step1.0FmeanQmax"]
+legends = ["TRPO","Neuroevolution","PSMCTS"]
+plot_name = exp_name+'_'+exp_param+'avgtop'+str(top_k)
 
 
 plts = []
-# legends = []
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(8, 8))
 
 for (policy_index,policy) in enumerate(policies):
     print(policy)
     Rewards = []
     min_array_length = np.inf
     for trial in range(n_trial):
-        file_path = prepath+exp+'/Data/AST/Lexington/'+policy+'/'+str(trial)+'/process.csv'
+        file_path = prepath+'/'+policy+'/'+str(trial)+'/process.csv'
         if os.path.exists(file_path):
             print(trial)
             steps = []
             rewards = []
             with open(file_path) as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=',')
+                if '\0' in open(file_path).read():
+                    print("you have null bytes in your input file")
+                    csv_reader = csv.reader(x.replace('\0', '') for x in csv_file)
+                else:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+
                 for (i,row) in enumerate(csv_reader):
                     if i == 0:
                         entry_dict = {}
                         for index in range(len(row)):
                             entry_dict[row[index]] = index
                     else:
+                        # print(row[entry_dict["StepNum"]])
                         if int(row[entry_dict["StepNum"]]) > max_step:
                             break
                         if int(row[entry_dict["StepNum"]])%batch_size == 0:
@@ -59,19 +62,14 @@ for (policy_index,policy) in enumerate(policies):
             if len(rewards) < min_array_length:
                 min_array_length = len(rewards) 
             Rewards.append(rewards)
-            # print(len(rewards))
-            # print(steps[-1])
-            # print(min_array_length)
     steps = steps[:min_array_length]
     Rewards = [rewards[:min_array_length] for rewards in Rewards]
-    # plot, = plt.plot(steps,np.mean(Rewards,0),color=colors[policy_index])
-    plot, = plt.plot(steps,np.mean(Rewards,0))
+    plot,_,_ = plt.errorbar(steps,np.mean(Rewards,0),yerr=np.std(Rewards,0)/np.sqrt(n_trial),errorevery=10)
     plts.append(plot)
     # legends.append(policy)
 
-plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-plt.legend(plts,legends,loc='lower right')
+plt.legend(plts,legends)
 plt.xlabel('Step Number')
-plt.ylabel('Average Top '+str(top_k) +' Reward')        
-fig.savefig(prepath+exp+'/Data/Plot/avgtop10/'+plot_name+'_avgtop10.pdf')
+plt.ylabel('Average Top '+str(top_k) +' Return')       
+fig.savefig(plot_path+plot_name,format='pdf')
 plt.close(fig)
