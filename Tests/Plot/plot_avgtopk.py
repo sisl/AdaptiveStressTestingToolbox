8361ec078fbbe5a929b1432ac60c334f289c1095
maxiaoba <maxiaoba@umich.edu>
2019-02-11 09:55:35 -0800

    add lunarlander R0U0

--- Tests/Plot/plot_avgtopk.py
+++ Tests/Plot/plot_avgtopk.py
@@ -5,7 +5,7 @@ matplotlib.rcParams.update({'font.size': 15})
 from matplotlib import pyplot as plt
 import numpy as np
 
-n_trial = 5
+n_trial = 4
 top_k = 1
 batch_size = 4000
 max_step = np.inf
@@ -13,7 +13,7 @@ max_reward = np.inf
 min_reward = -np.inf
 
 exp_name = 'LunarLander'#'CartPole'#'BipedalWalker'#'MountainCar'#'Acrobot'#
-exp_param = 'L100R100U0'#'L100Th0605It01'#'L100TL35'#'L100P00015'#'L100Th19999'#
+exp_param = 'L100R10U0'#'L100Th0605It01'#'L100TL35'#'L100P00015'#'L100Th19999'#
 prepath = "../"+exp_name+"/Data/AST/Lexington/"+exp_param
 plot_path = "../"+exp_name+"/Data/Plot/avgtop"+str(top_k)+"/"
 policies = ["TRPO",\
