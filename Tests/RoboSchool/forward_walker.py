1f0ec4acf82ef3312603fed54c2de938182b649d
maxiaoba <maxiaoba@umich.edu>
2019-05-15 11:20:56 -0700

    incarease done reward to be equal to the trajectory length

--- Tests/RoboSchool/forward_walker.py
+++ Tests/RoboSchool/forward_walker.py
@@ -118,7 +118,7 @@ class RoboschoolForwardWalker(SharedMemoryClientEnv):
 
         progress = 0.0
         if self.walk_target_dist < 1.0:
-            progress = 100.0
+            progress = 500.0
             done = True
             # print("seccess!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
