fa41bab39b062fe688a9a2656a80f26d002e2d42
mark-koren <mkorenfhspltw09@gmail.com>
2020-04-27 17:01:24 -0700

    Changed from unittest to pytest

--- TestCases/Tests/tests.py
+++ TestCases/Tests/tests.py
@@ -1,11 +1,16 @@
-import unittest
-
+# import unittest
+#
+# from TestCases.Tests.validate_install import validate_install
+#
+# class TestMethods(unittest.TestCase):
+#     def test_add(self):
+#         self.assertEqual(validate_install(), True)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
 from TestCases.Tests.validate_install import validate_install
 
-class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(validate_install(), True)
 
-
-if __name__ == '__main__':
-    unittest.main()
+def test_validate_install():
+    assert validate_install() is True
