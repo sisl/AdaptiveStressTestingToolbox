8ccd775676c463c884a5ac9dfcaeb3c3d28ccd35
mark-koren <mkorenfhspltw09@gmail.com>
2020-04-27 17:47:03 -0700

    added test of ASTSimulator base class

--- TestCases/Tests/tests.py
+++ TestCases/Tests/tests.py
@@ -9,8 +9,22 @@
 #
 # if __name__ == '__main__':
 #     unittest.main()
+import pytest
 from TestCases.Tests.validate_install import validate_install
 
 
 def test_validate_install():
     assert validate_install() is True
+
+from mylab.simulators.ast_simulator import ASTSimulator
+
+def test_ast_simulator():
+    sim = ASTSimulator()
+    with pytest.raises(NotImplementedError):
+        sim.simulate(None, None)
+        sim.step(None, None)
+        sim.reset(None)
+        sim.get_reward_info()
+        sim.is_goal()
+
+    assert sim.log() is None
