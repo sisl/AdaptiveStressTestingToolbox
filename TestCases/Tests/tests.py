70cba02d5d8b9c7a42e70723e72ad8db2a05777c
mark-koren <mkorenfhspltw09@gmail.com>
2020-04-28 10:17:24 -0700

    Adding ExampleAVSimulator test cases

--- TestCases/Tests/tests.py
+++ TestCases/Tests/tests.py
@@ -10,6 +10,7 @@
 # if __name__ == '__main__':
 #     unittest.main()
 import pytest
+import numpy as np
 from TestCases.Tests.validate_install import validate_install
 
 
@@ -32,3 +33,31 @@ def test_ast_simulator():
         sim.is_goal()
 
     assert sim.log() is None
+
+from mylab.simulators.example_av_simulator import ExampleAVSimulator
+
+def test_example_av_simulator():
+    sim = ExampleAVSimulator(car_init_x=0, car_init_y=0, max_path_length=1)
+
+#     Check if simulate can find a goal
+    path_length, info = sim.simulate(actions=np.zeros(6), s_0=np.array([0,0,0,0]))
+    assert path_length == 0
+    # Check if simulate can return end of path
+    path_length, info = sim.simulate(actions=np.zeros(6), s_0=np.array([5, 5, 0, 0]))
+    assert path_length == -1
+
+    # Check open-loop sim step
+    obs = sim.step(action=None, open_loop=True)
+    assert obs is None
+
+    # Check closed-loop sim step
+    init = sim.reset(s_0=np.array([0,0,0,0]))
+    assert np.all(init == np.array([0,0,0,0]))
+    obs = sim.step(action=np.zeros(6), open_loop=True)
+    print(obs)
+    assert np.all(init == np.array([0,0,0,0]))
+
+    # check white-box reset
+    sim.action_only = False
+    init = sim.reset(s_0=np.array([0, 0, 0, 0]))
+    assert np.all(init == np.array([0, 0, 0, 0]))
