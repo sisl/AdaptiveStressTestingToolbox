4a82b4741fd9ff6283e8bc79a3f9bb9e7ce69ba8
mark-koren <mkorenfhspltw09@gmail.com>
2020-05-20 12:09:30 -0700

    Added path changes

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,16 @@
 from __future__ import unicode_literals
 import os
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath('../'))
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
