907f958b97d69637cdbe7233b9442d9803a77308
mkoren <mkorenfhspltw09@gmail.com>
2020-09-03 10:38:24 -0700

    Change how bsddb3 dependencies work

--- src/ast_toolbox/algos/__init__.py
+++ src/ast_toolbox/algos/__init__.py
@@ -1,8 +1,11 @@
 """Algorithms for solving AST formulated RL problems"""
-from .backward_algorithm import BackwardAlgorithm  # noqa
+import importlib
+if importlib.util.find_spec('bsddb3') is not None:
+    # Only load bsddb3 dependent packages if it is installed
+    from .go_explore import GoExplore  # noqa
+    from .backward_algorithm import BackwardAlgorithm  # noqa
 from .ga import GA  # noqa
 from .gasm import GASM  # noqa
-from .go_explore import GoExplore  # noqa
 from .mcts import MCTS  # noqa
 from .mctsbv import MCTSBV  # noqa
 from .mctsrs import MCTSRS  # noqa
