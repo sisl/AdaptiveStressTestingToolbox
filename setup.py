81a240b8a904f43e79e18f91bd1c8309701645c7
mkoren <mkorenfhspltw09@gmail.com>
2021-06-02 12:04:37 -0700

    Officially only support python 3.6

--- setup.py
+++ setup.py
@@ -95,7 +95,8 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='=3.6',
+    # python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     setup_requires=[
